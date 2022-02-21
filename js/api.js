d7081f8bf9b060835704a913db8c8742320380e1
ahsan <makabir98@yahoo.com>
2022-02-21 21:32:30 +0600

    loaded the fetched data to a HTML element like li

--- js/api.js
+++ js/api.js
@@ -1,3 +1,26 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json));
+}
+
+function loadUsers(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data));
+}
+
+function loadPost(){
+    fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+function displayUsers(data){
+    const ul = document.getElementById('users');
+    for (const user of data){
+        console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
+}
