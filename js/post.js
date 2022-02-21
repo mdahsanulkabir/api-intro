617dd7637d3cc02b9aebc8d5931358fb02c137b1
ahsan <makabir98@yahoo.com>
2022-02-21 23:18:11 +0600

    the module done

--- js/post.js
+++ js/post.js
@@ -19,3 +19,19 @@ function displayPosts(posts){
 }
 
 loadPost();
+
+function addAPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My new post',
+            body: 'This is my post',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
